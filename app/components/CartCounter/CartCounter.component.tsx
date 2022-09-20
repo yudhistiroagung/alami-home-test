be52ad788eeb350dc4ecb355ae3e70e94f62077f
Yudhistiro Agung <yudhistiro.agung@gmail.com>
2022-09-20 22:28:04 +0700

    remove unused code

--- app/components/CartCounter/CartCounter.component.tsx
+++ app/components/CartCounter/CartCounter.component.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { View, Text, Button, Image } from 'react-native';
 
 import Images from '../../assets/images';
